135d3b0f5a86e88a017ed6aacc50046b91f5583f
renato <renatovp32@gmail.com>
2023-08-17 13:29:57 -0500

    Primer commit

---
+++ src/main/java/com/vivanco/ApiRestInventariomvcApplication.java
@@ -0,0 +1,13 @@
+package com.vivanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestInventariomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestInventariomvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivanco/ApiRestInventariomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestInventariomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
